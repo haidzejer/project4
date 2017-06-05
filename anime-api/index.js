acaa3b56fd66793b0bc14c83b2723ca00aeffd7a
Jordan Hall <jordanhall.1995@yahoo.com>
2017-06-05 12:45:29 -0700

    can do CRUD actions on Users in postman

--- anime-api/index.js
+++ anime-api/index.js
@@ -3,9 +3,15 @@ const
   app = express(),
   mongoose = require('mongoose'),
   logger = require('morgan'),
-  usersRoutes = require('./routes/users.js')
+  usersRoutes = require('./routes/users.js'),
+  mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/anime'
 
   const port = 3001
+
+  //
+  mongoose.connect(mongoUrl, (err) => {
+    console.log(err || "Connected to MongoDB.")
+  })
 
   app.get('/', (err) => {
     console.log("hello")
