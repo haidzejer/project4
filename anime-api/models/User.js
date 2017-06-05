62238391934c8828f1c67c694354174d3ef50fa5
Jeremiah Haidze <haidzejeremiah@yahoo.com>
2017-06-05 11:00:25 -0700

    added user model

--- anime-api/models/User.js
+++ anime-api/models/User.js
@@ -6,3 +6,22 @@ const
     email: String,
     password: {type: String, select: false}
   })
+
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8))
+}
+
+userSchema.methods.validPassword = function(password){
+  if(!password)return false
+  return bcrypt.compareSync(password, this.password)
+}
+
+userSchema.pre('save', function(next) {
+  const user = this
+  if(!user.isModified('password')) return next()
+  user.password = user.generateHash(user.password)
+  next()
+})
+
+
+module.exports = mongoose.model('User', userSchema)
