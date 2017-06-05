4a853d0c421fb3db4186ed1ed732dc4b523c6adf
Jordan Hall <jordanhall.1995@yahoo.com>
2017-06-05 11:22:46 -0700

    basic routes

--- anime-api/routes/users.js
+++ anime-api/routes/users.js
@@ -3,7 +3,7 @@ const
   usersRouter = new express.Router()
 
 
-usersRouter.port('/users', (req, res) => {
+usersRouter.post('/api/users', (req, res) => {
   User.create(req.body, (err, user) => {
     const userData.toObject()
     delete userData.password
@@ -12,3 +12,27 @@ usersRouter.port('/users', (req, res) => {
     res.json({success: true, message: "User account created.", user, token})
   })
 })
+
+usersRouter.route('/api/users/:id')
+  .get((req, res) => {
+    User.findById(req.params.id, (err, user) => {
+      res.json(user)
+    })
+  })
+  .patch((req, res) => {
+    User.findById(res.params.id, (err, user) => {
+      if(err) return console.log(err)
+      Object.assign(user, req.body)
+      user.save((err) => {
+        res.json({success: true, message: "User updated...", user: user})
+      })
+    })
+  })
+  .delete((req, res) => {
+    User.findByIdAndRemove(req.params.id, (err, user) => {
+      if(err) return console.log(err)
+      res.json({success: true, message: "User destroyed..."})
+    })
+  })
+
+module.exports = usersRouter
