07f815bb65f0198a0674af0ca93e13cd699dbffd
Jeremiah Haidze <haidzejeremiah@yahoo.com>
2017-06-06 20:03:26 -0700

    added marker to user's location

--- anime-client/src/Map.js
+++ anime-client/src/Map.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { GeoJSONLayer, ScaleControl, ZoomControl } from "react-mapbox-gl";
+import ReactMapboxGl, { GeoJSONLayer, ScaleControl, ZoomControl, Layer, Feature, Marker } from "react-mapbox-gl";
 import clientAuth from './clientAuth';
 
 const currentUser = clientAuth.getCurrentUser()
 const accessToken = "pk.eyJ1IjoiamVyZW1pYWhoIiwiYSI6ImNqM2t2d3duYTAwc3MycXJ6ZTk3N2ttemEifQ.GRIn6Jx-V76v9R9vPtT-HQ";
-const style = "mapbox://styles/mapbox/streets-v9";
+const style = "mapbox://styles/mapbox/dark-v9";
 
 const containerStyle = {
   height: "100vh",
@@ -17,11 +17,15 @@ export default class Map extends Component {
     center: [currentUser.lng, currentUser.lat]
   };
 
+  _onClickMarker() {
+    console.log("marker clicked!")
+  }
+
   render() {
     return (
       <ReactMapboxGl
         // eslint-disable-next-line
-        style="mapbox://styles/mapbox/dark-v9"
+        style={style}
         accessToken={accessToken}
         center={this.state.center}
         movingMethod="jumpTo"
@@ -29,6 +33,34 @@ export default class Map extends Component {
 
         <ScaleControl/>
         <ZoomControl/>
+
+
+        <Layer
+          type="symbol"
+          layout={{ "icon-image": "marker-15" }}>
+          <Feature
+            coordinates={[currentUser.lng, currentUser.lat]}
+            onHover={this._onHover}
+            onEndHover={this._onEndHover}
+            onClick={this._onClickMarker}/>
+        </Layer>
+
+        {/* <Layer
+          type="symbol"
+          id="marker"
+          layout={{ "icon-image": "marker-15" }}>
+          <Feature coordinates={[currentUser.lng, currentUser.lat]}/>
+        </Layer> */}
+
+        {/* <Marker
+          onClick={this._markerClick}
+          anchor="top"
+          onMouseEnter={this._markerMouseEnter}
+          onMouseLeave={this._markerMouseLeave}
+          className="test"
+          coordinates={currentUser.lng, currentUser.lat}>
+            <h1>TEST</h1>
+        </Marker> */}
 
 
       </ReactMapboxGl>
