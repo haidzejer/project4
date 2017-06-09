55809c91dfc5843a1e777c7bfdc2fb71addee432
Jeremiah Haidze <haidzejeremiah@yahoo.com>
2017-06-08 21:45:39 -0700

    added styles

--- anime-client/src/Chat.js
+++ anime-client/src/Chat.js
@@ -27,11 +27,13 @@ class Chat extends Component {
     const socket = this.props.socket
     var message = this.refs.message.value
     socket.emit('chat-message', message)
-    console.log(this.state.messages)
     this.refs.message.value = ''
   }
 
   render() {
+    const messages = this.state.messages.map((m,i) => {
+      return <li>{m}</li>
+    })
 
     // socket.emit('test', "emitting message from chat component")
     console.log(this.state)
@@ -39,7 +41,12 @@ class Chat extends Component {
       <div id="chat-container">
         <div id="project-manger-is-stupid">
           <h1>I think it's working!!!</h1>
-          <p id="hi">{this.state.messages}</p>
+          {console.log(this.state.messages)}
+          <div className="chatBox">
+            <ul className="message">
+              {messages}
+            </ul>
+          </div>
         </div>
         <input id="input" ref="message" type="text" />
         <button id="send-message" onClick={this._sendMessage.bind(this)} required='true'>Send your bitch ass message</button>
