8150530f41dba8578338dd64d3e29c805f6e59ac
Jeremiah Haidze <haidzejeremiah@yahoo.com>
2017-06-09 10:07:10 -0700

    server port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const
   io = require('socket.io')(server),
   dotenv = require('dotenv').load({silent: true})
 
-  const port = 3001
+  const port = process.env.PORT || 3001
 
 
   mongoose.connect(mongoUrl, (err) => {
